b3d4baa7f4e14b2b7da3e281c3a7a26230a08420
Zi Jian <yeezj.24@ichat.sp.edu.sg>
2025-05-22 00:36:05 +0800

    lab4 modified

--- src/PiLedTest.py
+++ src/PiLedTest.py
@@ -1,19 +1,20 @@
 import hal.hal_input_switch as input_switch
 import hal.hal_led as led
 import time 
-
+import PiDemo as blink
+i=0
 def main():
     led.init()
     input_switch.init()
 
     switchValue=input_switch.read_slide_switch()
     if switchValue==1:
-        led.set_output(24,1)
-        time.sleep(0.1)
-        led.set_output(24,0)
-        time.sleep(0.1)
+       while switchValue==1:
+        blink.blink_led(0.1)
     else:
-       led.set_output(24,0)
+       while (i<25):
+        blink.blink_led(0.05)
+        i+=1
 
 if __name__=="__main__":
  main()
